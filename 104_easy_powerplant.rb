35c7cdc017b661555f308655d3aebcafcaa5ea59
Fernando Paredes <nanoid.xd@gmail.com>
2013-04-20 21:26:14 -0700

    Finish Challenge 122 (Easy)

---
+++ 104_easy_powerplant.rb
@@ -0,0 +1,13 @@
+# Description:
+# A powerplant for the city of Redmond goes offline every third day because of local demands. Ontop of this, the powerplant has to go offline for maintenance every 100 days. Keeping things complicated, on every 14th day, the powerplant is turned off for refueling. Your goal is to write a function which returns the number of days the powerplant is operational given a number of days to simulate.
+# Formal Inputs & Outputs:
+# Input Description:
+# Integer days - the number of days we want to simulate the powerplant
+# Output Description:
+# Return the number of days the powerplant is operational.
+# Sample Inputs & Outputs:
+# The function, given 10, should return 7 (3 days removed because of maintenance every third day).
+
+def num_days_online(days)
+  return (1..days).inject(0) { |sum, i| (i%100==0 or i%14==0 or i%3==0) ? sum : sum + 1 }
+end
